a492f7be827acf25a6b957507e716c44a8fed74a
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-20 20:10:04 +0530

    added test to check if file data is a string

--- test/unit/utils/FileReader.test.js
+++ test/unit/utils/FileReader.test.js
@@ -10,4 +10,9 @@ describe("File Reader Tests", () => {
     test('successfully read a file', async () => {
         assert.doesNotReject(readSourceFile(filePath));
     });
+
+    test('file data should be a string', async () => {
+        const data = await readSourceFile(filePath);
+        assert.strictEqual(typeof data, "string");
+    });
 });
