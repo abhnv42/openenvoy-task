9363605771b431825f185fb335c912520e2083e5
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-20 20:08:20 +0530

    moved filepath to upper scope

--- test/unit/utils/FileReader.test.js
+++ test/unit/utils/FileReader.test.js
@@ -4,11 +4,10 @@ import readSourceFile from "../../../src/utils/FileReader.js";
 import { describe, test } from "node:test";
 import { strict as assert } from "node:assert";
 
+const filePath = new URL('../../sourceFiles/example.source.java', import.meta.url);
+
 describe("File Reader Tests", () => {
     test('successfully read a file', async () => {
-        const filePath = new URL('../../sourceFiles/example.source.java', import.meta.url);
         assert.doesNotReject(readSourceFile(filePath));
     });
-
-    test('')
 });
