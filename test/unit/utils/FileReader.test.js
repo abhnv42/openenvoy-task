e5460533235bba38ba8cd0a116fc649fa3666ce0
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-20 20:06:47 +0530

    corrected file read success test

--- test/unit/utils/FileReader.test.js
+++ test/unit/utils/FileReader.test.js
@@ -7,7 +7,8 @@ import { strict as assert } from "node:assert";
 describe("File Reader Tests", () => {
     test('successfully read a file', async () => {
         const filePath = new URL('../../sourceFiles/example.source.java', import.meta.url);
-        const data = await readSourceFile(filePath);
-        assert.strictEqual(typeof data, "string");
+        assert.doesNotReject(readSourceFile(filePath));
     });
+
+    test('')
 });
