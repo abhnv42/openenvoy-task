a716b2d981e2e4bd3ca5a9b284b4d6498831795b
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 13:08:54 +0530

    updated error message in a test

--- test/unit/utils/FileReader.test.js
+++ test/unit/utils/FileReader.test.js
@@ -17,6 +17,7 @@ describe("File Reader Tests", () => {
     });
 
     test('should throw an error if filePath is invalid', async () => {
-        assert.rejects(readSourceFile(""), { message: "Invalid file path" });
+        const filePath = "";
+        assert.rejects(readSourceFile(filePath), { message: `File not found: ${filePath}` });
     })
 });
