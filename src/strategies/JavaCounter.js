9968d59484331a9bf212e59bbad4b4f7fbeb4ac5
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 20:14:15 +0530

    lifted up counter methods to parent class

--- src/strategies/JavaCounter.js
+++ src/strategies/JavaCounter.js
@@ -1,44 +1,7 @@
-import config from "../../config/config.js";
 import { LanguageCounter } from "./LanguageCounter.js"
 
 export class JavaCounter extends LanguageCounter {
     constructor(fileData, extension) {
-        super();
-        this.originalData = fileData;
-        this.extension = extension.split(".").join("");
-        this.lines = this.preprocess(fileData);
-    }
-
-    countLines() {
-        let comments = 0;
-        let blankLines = 0;
-        let loc = 0;
-
-        for(let i = 0; i < this.lines.length; i++) {
-            const currentLine = this.lines[i];
-            if(this.isBlankLine(currentLine)) ++blankLines;
-            if(this.isComment(currentLine)) ++comments;
-            if(this.isLoc(currentLine)) ++loc;
-        }
-
-        return {blankLines, comments, loc};
-    }
-
-    preprocess(data) {
-        return data
-            .split("\n")
-            .map(line => line.replace(/\t/g, "").trim());
-    }
-
-    isBlankLine(line) {
-        return line.length === 0;
-    }
-
-    isComment(line) {
-        return line.match(config.syntaxInfo.java.commentRegex)
-    }
-
-    isLoc(line) {
-        return !this.isBlankLine(line) && !this.isComment(line);
+        super(fileData, extension);
     }
 }
--- src/strategies/LanguageCounter.js
+++ src/strategies/LanguageCounter.js
@@ -1,22 +1,43 @@
+import config from "../../config/config.js";
+
 export class LanguageCounter {
 
-    preprocess() {
-        throw new Error("Method not implemented");
+    constructor(fileData, extension) {
+        this.originalData = fileData;
+        this.extension = extension.split(".").join("");
+        this.lines = this.preprocess(fileData);
+    }
+
+    preprocess(data) {
+        return data
+            .split("\n")
+            .map(line => line.replace(/\t/g, "").trim());
     }
 
     countLines() {
-        throw new Error("Method not implemented");
+        let comments = 0;
+        let blankLines = 0;
+        let loc = 0;
+
+        for(let i = 0; i < this.lines.length; i++) {
+            const currentLine = this.lines[i];
+            if(this.isBlankLine(currentLine)) ++blankLines;
+            if(this.isComment(currentLine)) ++comments;
+            if(this.isLoc(currentLine)) ++loc;
+        }
+
+        return {blankLines, comments, loc};
     }
 
-    isBlankLine() {
-        throw new Error("Method not implemented");
+    isBlankLine(line) {
+        return line.length === 0;
     }
 
-    isComment() {
-        throw new Error("Method not implemented");
+    isComment(line) {
+        return line.match(config.syntaxInfo[this.extension].commentRegex)
     }
 
-    isLoc() {
-        throw new Error("Method not implemented");
+    isLoc(line) {
+        return !this.isBlankLine(line) && !this.isComment(line);
     }
 }
