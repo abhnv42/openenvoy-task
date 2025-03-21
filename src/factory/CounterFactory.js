d4630d087c49793c14b3c618a36ace1400d5d4db
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 20:02:57 +0530

    added implementation to Counter factory

--- src/factory/CounterFactory.js
+++ src/factory/CounterFactory.js
@@ -1,3 +1,13 @@
-export class CounterFactory {
-    
+import {JavaCounter} from "../strategies/JavaCounter.js";
+import readSourceFile from "../utils/FileReader.js";
+
+export class Counter {
+    static async getCounter(filePath) {
+        const {data, extension} = await readSourceFile(filePath);
+        switch(extension) {
+            case ".java": {
+                return new JavaCounter(data, extension);
+            }
+        }
+    }
 }
