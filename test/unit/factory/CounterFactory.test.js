8353a382072ad8670225133b968f0984c78e5aff
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 19:41:18 +0530

    added test to verify factory method

---
+++ test/unit/factory/CounterFactory.test.js
@@ -0,0 +1,13 @@
+import {describe, test} from "node:test";
+import {strict as assert} from "node:assert";
+
+import { Counter } from "../../../src/factory/CounterFactory.js";
+
+const javaFilePath = new URL("../../sourceFiles/example.source.java", import.meta.url);
+
+describe("Counter", () => {
+    test("get correct counter based on file extension", async () => {
+        const counter = new Counter(javaFilePath);
+        assert(counter.language, "java");
+    });
+})
