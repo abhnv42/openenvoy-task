f373a6cd128e3b19927f185ce0a277ebcd76b402
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 16:25:12 +0530

    renamed LanguageCounter test suite

--- test/unit/strategies/LanguageCounter.test.js
+++ test/unit/strategies/LanguageCounter.test.js
@@ -5,8 +5,8 @@ import { LanguageCounter } from "../../../src/strategies/LanguageCounter.js";
 import {describe, test} from "node:test";
 import {strict as assert} from "node:assert";
 
-describe("LanguageCounter tests", () => {
-    test("LanguageCounter exists", () => {
+describe("LanguageCounter", () => {
+    test("should be defined", () => {
         assert.ok(LanguageCounter);
     });
 
