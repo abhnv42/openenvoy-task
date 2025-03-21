85236e4fd2afcdcc62dec784d7ae7a53730ae7f0
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 20:34:46 +0530

    added an integration test

---
+++ test/integration/final.test.js
@@ -0,0 +1,25 @@
+import {describe, test} from "node:test";
+import {strict as assert} from "node:assert";
+import { extname } from "node:path";
+
+import config from "../../config/config.js"
+import {Counter} from "../../src/Counter.js"
+import testFileMetaData from "../sourceFiles/testFileMetaData.js";
+
+const supportedFiles = Object.keys(testFileMetaData).filter(key => config.supportedExtensions.includes(extname(key)));
+
+for(const fileName of supportedFiles) {
+    const expected = testFileMetaData[fileName];
+    const filePath = new URL(`../sourceFiles/${fileName}`, import.meta.url);
+    
+    describe(`Integration Test: ${fileName}`, () => {
+        test("check counts", async () => {
+            const counter = await Counter.getCounter(filePath);
+            const result = counter.countLines();
+
+            assert.equal(result.blankLines, expected.blankLines);
+            assert.equal(result.comments, expected.comments);
+            assert.equal(result.loc, expected.loc);
+        })
+    });
+}
