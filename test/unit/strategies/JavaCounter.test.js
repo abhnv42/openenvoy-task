9dfb3b090d7fe082e883153cea13f3b799599536
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 16:59:59 +0530

    added test to check preprocess JavaCounter.js

--- test/unit/strategies/JavaCounter.test.js
+++ test/unit/strategies/JavaCounter.test.js
@@ -1,10 +1,19 @@
 import { JavaCounter } from "../../../src/strategies/JavaCounter.js";
+import readSourceFile from "../../../src/utils/FileReader.js";
 
 import {describe, test} from "node:test";
 import {strict as assert} from "node:assert";
+
+const filePath = new URL("../../sourceFiles/example.source.java", import.meta.url);
 
 describe("JavaCounter", () => {
     test("should be defined", () => {
         assert.ok(JavaCounter);
     });
+
+    test("preprocess should work correctly", async () => {
+        const data = await readSourceFile(filePath);
+        const javaCounter = new JavaCounter(data);
+        assert.ok(Array.isArray(javaCounter.lines));
+    })
 })
