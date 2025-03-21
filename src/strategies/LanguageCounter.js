b1113293d2f9428481b707972a6056a96de840b7
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-21 16:25:40 +0530

    added empty respective type of line counting methods

--- src/strategies/LanguageCounter.js
+++ src/strategies/LanguageCounter.js
@@ -2,4 +2,16 @@ export class LanguageCounter {
     countLines() {
         throw new Error("Method not implemented");
     }
+
+    countBlankLines() {
+        throw new Error("Method not implemented");
+    }
+
+    countComments() {
+        throw new Error("Method not implemented");
+    }
+
+    countLoc() {
+        throw new Error("Method not implemented");
+    }
 }
